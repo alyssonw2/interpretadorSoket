34f942913d287eb35e8096a16919f425b854d22f
Alyssonw2 <blogdoalysson@gmail.com>
2022-11-19 13:25:42 -0300

    install express definindo rotas

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import * as dotenv from 'dotenv' 
+import express from 'express'
 dotenv.config()
 //console.log(process.env.IP)
-//console.log(process.env.PORTA)
 //console.log(process.env.WS)
+const app = express()
+const port = process.env.PORTA
+//importando rotas 
+import './rotas/index.js'
+
+app.listen(port, () => {
+  console.log(`Rodando interpretador na porta: ${port}`)
+})
